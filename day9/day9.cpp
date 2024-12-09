a2e6efa4c1fa47fad572aea8cc943c840b2f0dec
MattR2718 <matthew.ranson2718@gmail.com>
2024-12-09 19:34:07 +0000

    faster p1

--- day9/day9.cpp
+++ day9/day9.cpp
@@ -8,6 +8,13 @@ struct entry {
 	int width;
 };
 
+void print(const std::vector<int>& v) {
+	for (const int i : v) {
+		std::cout << (i == 0 ? "." : std::to_string(i - 1));
+	}
+	std::cout << '\n';
+}
+
 int main() {
 	
 	INITIALIZE_AOC_TIMERS();
@@ -43,16 +50,18 @@ int main() {
 	default_timer.begin(1);
 	uint64_t p1 = 0;
 
-	for (int i = in.size() - 1; i > 0; i--) {
-		if (in[i] != 0) {
-			for (int j = 0; j < i; j++) {
-				if (in[j] == 0) {
-					in[j] = in[i];
-					in[i] = 0;
+	size_t p = 0;
+	const size_t n = in.size();
 
-					break;
-				}
+	for (size_t i = n - 1; i > 0; i--) {
+		if (in[i] != 0) {
+			while (p < i && in[p] != 0) {
+				p++;
 			}
+			if (p >= i) break;
+
+			in[p] = in[i];
+			in[i] = 0;
 		}
 	}
 
@@ -71,7 +80,7 @@ int main() {
 	f = false;
 	id = 0;
 	for (const char c : sv) {
-			fs.emplace_back(entry{ (f = !f, f), (f ? id++ : -1), c - '0'});
+		fs.emplace_back(entry{ (f = !f, f), (f ? id++ : -1), c - '0'});
 	}
 
 	for (int i = fs.size() - 1; i > 0; i--) {
@@ -106,3 +115,39 @@ int main() {
 
 	default_timer.display_all();
 }
+
+
+//Part 1: 6399153661894
+//Part 2 : 6421724645083
+//============================== Timer Details ==============================
+//Timer ID : 0
+//Label : Input
+//Description : Read input from file and parse
+//Elapsed Time : 581.9 microseconds
+//========================================================================== =
+//============================== Timer Details ==============================
+//Timer ID : 1
+//Label : Part 1
+//Description : Compute part 1
+//Elapsed Time : 208.9 microseconds
+//========================================================================== =
+//============================== Timer Details ==============================
+//Timer ID : 2
+//Label : Part 2
+//Description : Compute part 2
+//Elapsed Time : 102833.7 microseconds
+//========================================================================== =
+//
+//
+//
+//Days: 0
+//Hours : 0
+//Minutes : 0
+//Seconds : 0
+//Milliseconds : 224
+//Ticks : 2245607
+//TotalDays : 2.59908217592593E-06
+//TotalHours : 6.23779722222222E-05
+//TotalMinutes : 0.00374267833333333
+//TotalSeconds : 0.2245607
+//TotalMilliseconds : 224.5607
